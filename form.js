71f80f821214271180da08e651d2ec9503a6b5b0
WebMasterZat <allesklar777@googlemail.com>
2021-09-08 20:36:37 +0200

    smena statusa cherez checkbox

---
+++ form.js
@@ -0,0 +1,63 @@
+class FormClass {
+    constructor() {
+        this.form = document.forms[0]
+        this.errorsField = this.form.querySelector('.error')
+    }
+    fields = {
+        title: 'Заголовок',
+        body: 'Содержание',
+        test: 'ok',
+        test2: 'ok2'
+    }
+    // [title, body]
+    // ['Заголовок', 'Содержание']
+
+    // [title]
+    // ['Заголовок']
+
+    // сбросить все поля формы до значений по умолчанию (метод объекта forms браузера)
+    reset(){
+        this.form.reset()
+    }
+
+    //
+    errorsReset(){
+        // удаляет информацию из секции с классом error (список не заполненных полей)
+        this.errorsField.innerHTML = null
+    }
+
+    //
+    errors(b) {
+        // ВАРИАНТ 1
+        console.log('wrongFields', b)
+        let a = []
+        for (let i = 0; i < b.length; i++){
+            let field = b[i]
+/*            console.log('b: ',b)
+            console.log('i: ',i)
+            console.log('b[i]: ',b[i])*/
+
+            // this.fields['title'] = Заголовок
+            a.push(this.fields[field])
+        }
+        // ВАРИАНТ 2
+        /*
+        let a = b.reduce((acc, cur) => {
+            acc.push(this.fields[cur])
+            return acc
+        },[])
+        */
+
+        // формирование сообщения о предупреждении, что отсутствует информация в поле или полях формы
+        document.forms[0].querySelector('.error').innerHTML =
+            `
+               <div class="alert alert-danger  my-4">
+                     <p>Заполните пустые поля</p>
+                     <ul>
+                            ${a.map(field => { return `<li>${field}</li>`})
+                            .join('')}
+                     </ul>
+               </div>
+            `
+    }
+}
---
+++ notes.js
@@ -0,0 +1,204 @@
+class NotesClass {
+    constructor() {
+        this.form = new FormClass
+        console.log('this.form: ', this.form)
+    }
+
+    add(note) {
+
+        // ВАЛИДАЦИЯ
+        //if (!this.validation(note)) return
+
+        this.validation(note)
+
+        const newNote = {
+            id: uuid.v4(),
+            completed: false,
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+            ...note
+            // title: note.title,
+            // body: note.body,
+        }
+        const notes = this.notes // массив объектов заметок
+        notes.push(newNote)
+        this.save(notes)
+        this.render()
+        console.log('Заметка добавлена')
+        this.form.reset()
+    }
+
+    // сохранить заметку в localstorage
+    save(notes) {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }
+
+    // получаем из localstorage объект заметки
+    get notes() {
+        return localStorage.getItem("notes") ? JSON.parse(localStorage.getItem("notes")) : []
+    }
+
+    // удалить заметку по id
+    remove(id) {
+        const notes = this.notes()
+        const index = notes.findIndex(note => note.id === id)
+        if (index !== -1) {
+            notes.splice(index, 1)
+            this.save(notes)
+        }
+    }
+
+
+    update(id, note) {
+
+        this.validation(note)
+
+        const notes = this.notes()
+        notes.forEach(item => {
+            if (item.id === id) {
+                item.title = note.title
+                item.body = note.body
+            }
+        })
+        this.save(notes)
+        console.log(`Заметка c id = ${id} обновлена`)
+        this.form.reset()
+
+        // ВАЛИДАЦИЯ
+        // if (!this.validation) ...
+
+        // обновление отдельной заметки
+        // получить из хранилища ...
+    }
+
+    // получаем id заметки
+    getById(id) {
+        // возврат заметки по id
+        return this.notes.find(el => el.id === id)
+    }
+
+
+    getByTitle(_title) {
+        // возврат заметки по id
+        return this.notes.filter(el => el.title.includes(_title))
+    }
+
+    // ПРИВАТНЫЙ МЕТОД - ВЫЗЫВАЕТСЯ ТОЛЬКО ВНУТРИ КЛАССА
+
+    // проверка введённых полей на наличие данных
+    validation(note) {
+        // удаляем список всех ошибок в классе .error
+        this.form.errorsReset()
+        // сохраняем в переменную keys все ключи из объекта note
+        let keys = Object.keys(note)
+        // создаём пустой массив
+        let wrongFields = []
+        // находим все пустые поля в форме отсекая пробелы с справа и слева
+        keys.forEach(key => {
+            if (note[key].trim() === '') {
+                wrongFields.push(key)
+            }
+        })
+        // и если в массиве есть хотя бы один элемент, который указывает на незаполненное поле то
+        // выдаётся сообщение
+        if (wrongFields.length > 0) {
+            console.log('Валидация не пройдена')
+            // функция error получает на вход объект (в зависимости от незаполненного поля), значения которого заносятся в новый массив,
+            //  потом значения этого массива формируют структуру html и сообщение об ошибки
+            this.form.errors(wrongFields)
+
+            // ПРЕКРАЩЕНИЕ РАБОТЫ ПРОГРАММЫ
+            throw new Error('Пустые поля формы')
+        }
+    }
+
+    // print
+    // вывод текущего (временного) массива заметок в консоль
+    // аргументы: -
+
+    print() {
+        console.log('print: ', this.notes)
+    }
+
+    // ВЫВОД И ПЕРЕРИСОВКА СПИСКА ЗАМЕТОК
+    // ПРИ ДОБАВЛЕНИИ ИЗ ФОРМЫ ЗАМЕТКА ДОЛЖНА ВЫВОДИТЬСЯ В КОНЦЕ СПИСКА ЗАМЕТОК НА ЭКРАНЕ
+    render() {
+        let notesList = document.querySelector('.notes-list')
+        const notes = this.notes
+
+
+        let ul = document.createElement('ul')
+
+        notesList.innerText = ''
+
+        notes.forEach((item, index) => {
+            let inputNotesList = document.createElement('input')
+            let labelNotesList = document.createElement('label')
+            inputNotesList.setAttribute('name', 'labelNotesList')
+            labelNotesList.setAttribute('for', 'labelNotesList' + index)
+
+
+
+            // 1. по клику на кнопку редактировать перейти на страницу edit
+
+            // 2. get (параметр search об-та location) параметром передать id заметки в формате id = номер id (сам id)
+
+            // 3. в самом файле edit.html в JS прописать получение get параметра id из строки браузера (он же получается из свойства search объекта location)
+            // т.е. если сторока вида http://test.localhost/edit.html?id=123
+            // то надо создать об-т query в формате { id: 123 }
+            // импользовать метод split или метод match ( c регулярным выражением )
+
+            // 4. Создать экземпляр класса заметок и по полученному из строки id получить текущую заметку из localstorage по её id (getById())
+            
+            // 5. поле заголовок найденной заметки поставить в value input формы редактирования, поле содержание в textarea. Т.е. вставить данные
+            // заметки в ДОМ
+            
+            // ЕСЛИ СМОЖЕШЬ
+            // 6. Отредактировать выбранную заметку и вернуться на страницу index.html, где заметка будет оборажаться в отредактирвоанном виде
+
+
+            let button = document.createElement('button')
+            button.innerText = 'Редактировать'
+            button.classList.add('btn')
+            button.classList.add('btn-success')
+            button.addEventListener('click', () => {
+                // location.href = 'http://test.localhost/edit.html'
+                console.log(location)
+
+                // const origin = location.origin
+                const { origin } = location
+                // location.href = `${origin}/edit.html`
+
+            })
+
+            inputNotesList.type = 'checkbox'
+            inputNotesList.id = 'labelNotesList' + index
+            inputNotesList.className = 'labelNotesList'
+            inputNotesList.checked = item.completed
+            notesList.appendChild(ul)
+            let li = document.createElement('li')
+            li.classList.add('my-3')
+            ul.appendChild(li).innerText = 'TITLE: ' + item.title + ' | ' + 'BODY: ' + item.body + ' | ' + 'COMPLETED: ' + item.completed
+            li.appendChild(inputNotesList)
+            li.appendChild(labelNotesList)
+            li.appendChild(button)
+
+            inputNotesList.addEventListener('change', (e) => {
+                item.completed = e.target.checked
+                this.save(notes)
+                this.render()
+            })
+
+        })
+    }
+}
+
+
+/*
+добавь к выводимой заметке чекбокс, который будет отображать статус поля completed
+
+true - чекбокс отмечен, false - нет
+
+поставь на него обработчик события, который будет переключать это поле у конкретной заметки
+
+не забывай включать перерисовку*/
